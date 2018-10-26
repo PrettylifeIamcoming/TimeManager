24ecfe704ab21985b6c5ea22958096af7fc4ca9c
Mike <729186076@qq.com>
2018-10-27 00:26:34 +0800

    Change to AndroidX.

--- app/src/main/java/com/prettylifeiamcoming/timemanager/fragment/MainTaskFragment.java
+++ app/src/main/java/com/prettylifeiamcoming/timemanager/fragment/MainTaskFragment.java
@@ -1,7 +1,6 @@
 package com.prettylifeiamcoming.timemanager.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +9,8 @@ import android.widget.TextView;
 
 import com.prettylifeiamcoming.timemanager.R;
 import com.prettylifeiamcoming.timemanager.bean.Task;
+
+import androidx.fragment.app.Fragment;
 
 public class MainTaskFragment extends Fragment {
     private Task mTask;
