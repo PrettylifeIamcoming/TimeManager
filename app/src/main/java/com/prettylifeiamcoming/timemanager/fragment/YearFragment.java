b7a516cfe9a61c9b084ceda5686c280dcce99f4c
Mike <729186076@qq.com>
2019-04-11 20:06:51 +0800

    Add yearview.

--- app/src/main/java/com/prettylifeiamcoming/timemanager/fragment/YearFragment.java
+++ app/src/main/java/com/prettylifeiamcoming/timemanager/fragment/YearFragment.java
@@ -1,23 +1,41 @@
 package com.prettylifeiamcoming.timemanager.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.prettylifeiamcoming.timemanager.R;
 
+import com.github.prettylife.yearview.CalendarView;
+
+import java.util.Calendar;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class YearFragment extends Fragment {
+public class YearFragment extends Fragment implements CalendarView.OnYearChangeListener {
+    private TextView textView;
+    private CalendarView calendarView;
 
-    public YearFragment() {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_year,container,false);
+        textView = view.findViewById(R.id.year);
+        textView.setText(String.valueOf(Calendar.getInstance().get(Calendar.YEAR)));
+        calendarView = view.findViewById(R.id.calendarView_year);
+        calendarView.showYearSelectLayout(Calendar.getInstance().get(Calendar.YEAR));
+        calendarView.setOnYearChangeListener(this);
 
+        return view;
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_year, container, false);
+    public void onYearChange(int year) {
+        textView.setText(String.valueOf(year));
     }
 }
